4da4fd1a1a223d31221f69b7a1b78f2dda643169
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2020-06-05 15:47:36 +0200

    Update Trackbar_control.hpp

--- source/library/gui/controls/comctl32.dll/Trackbar_control.hpp
+++ source/library/gui/controls/comctl32.dll/Trackbar_control.hpp
@@ -9,16 +9,6 @@ namespace winapi::gui {
     class Trackbar_control:
         public Extends_<Control>
     {
-    protected:
-        class Api_window_factory:
-            public Extends_<Base_::Api_window_factory>
-        {
-        public:
-            auto windowclass() const
-                -> Windowclass_id override
-            { return TRACKBAR_CLASS; }
-        };
-
     public:
         struct Styles{ enum Enum{
             vertical            = 0x01,
@@ -29,6 +19,7 @@ namespace winapi::gui {
             _ }; };
         static constexpr int n_styles = Styles::_;
 
+    private:
         static auto creation_style_bits_from( const Styles::Enum styles )
             -> WORD
         {
@@ -47,25 +38,45 @@ namespace winapi::gui {
                 bits |= (is_vertical? TBS_TOP : TBS_LEFT);
             } else if( has_ticks_dr ) {
                 bits |= (is_vertical? TBS_BOTTOM : TBS_RIGHT);
+            } else {
+                bits |= TBS_NOTICKS;
             }
             return bits;
         }
 
+    protected:
+        class Api_window_factory:
+            public Extends_<Base_::Api_window_factory>
+        {
+        public:
+            auto windowclass() const
+                -> Windowclass_id override
+            { return TRACKBAR_CLASS; }
+        };
+
+    public:
         Trackbar_control(
             const Type_<Displayable_window*>    p_parent,
             const POINT&                        position,
             const SIZE&                         size,
             const Styles::Enum                  styles  = {}
-            ):
+        ):
             Base_( tag::Wrap(), Api_window_factory().new_api_window(
                 p_parent->handle(), position, size, creation_style_bits_from( styles ) )
-                )
+            )
         {}
 
         Trackbar_control( tag::Wrap, Window_owner_handle window_handle ):
             Base_( tag::Wrap(), move( window_handle ) )
         {}
-
     };
+
+    inline auto operator|(
+        const Trackbar_control::Styles::Enum    a,
+        const Trackbar_control::Styles::Enum    b
+        ) -> Trackbar_control::Styles::Enum
+    {
+        return static_cast<Trackbar_control::Styles::Enum>( +a | +b );
+    }
 
 }  // namespace winapi::gui
